120fda8dfc3f0d959f49a5c7ecb8c5b23ee751fc
Sharat Hegde <sharath@testvagrant.com>
2024-01-02 20:38:58 +0530

    modified gradle.yml

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,2 +1,31 @@
-package pages;public class LoginPage {
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div[1]")
+    WebElement titel;
+    @FindBy(id = "user-name")
+    WebElement username;
+    @FindBy(id = "password")
+    WebElement password;
+    @FindBy(id = "login-button")
+    WebElement loginBtn;
+    public Boolean verifyPageTitle(String heading){
+       return titel.getText().equals(heading);
+    }
+    public void enterCredentials(String id, String pwd) {
+        username.sendKeys(id);
+        password.sendKeys(pwd);
+    }
+    public void clickLoginBtn(){
+        loginBtn.click();
+    }
+    public LoginPage (final WebDriver driver){
+        PageFactory.initElements(driver,this);
+    }
 }
--- src/test/java/LoginPageTest.java
+++ src/test/java/LoginPageTest.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class LoginPageTest {
+import driver.DriverCreator;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class LoginPageTest {
+    DriverCreator driverCreator;
+    WebDriver driver;
+    LoginPage loginPage;
+    @BeforeClass
+    public void setup(){
+        driverCreator = new DriverCreator();
+        driver = driverCreator.create("");
+        driver.get("https://www.saucedemo.com/");
+        loginPage = new LoginPage(driver);
+    }
+    @Test
+    public void isHeadingPresent(){
+        assertTrue(loginPage.verifyPageTitle("Swag Labs"));
+    }
+    @Test
+    public void isLoginSuccesfull(){
+        loginPage.enterCredentials("standard_user","secret_sauce");
+        loginPage.clickLoginBtn();
+        assertEquals(driver.findElement(By.xpath("//*[@id=\"header_container\"]/div[1]/div[2]/div")).getText(),"Swag Labs");
+    }
 }

7fc5cb94deb6f0b666c9a1a876123d0028cddc3d
Sharat Hegde <sharath@testvagrant.com>
2024-01-02 21:52:45 +0530

    chnaging workflow from ubantu to windows

--- src/main/java/driver/DriverCreator.java
+++ src/main/java/driver/DriverCreator.java
@@ -1,48 +1,29 @@
 package driver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+import io.github.bonigarcia.wdm.managers.EdgeDriverManager;
+import io.github.bonigarcia.wdm.managers.FirefoxDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.Objects;
 
 public class DriverCreator {
-
     public WebDriver create(String browser) {
         browser = setDefaultBrowser(browser);
         switch (browser.toLowerCase()) {
             case "firefox":
-                return createFirefoxDriver();
+                return new FirefoxDriverManager().create();
             case "edge":
-                return createEdgeDriver();
+                return new EdgeDriverManager().create();
             default:
-                return createChromeDriver();
+                WebDriver webDriver = new ChromeDriverManager().create();
+                return webDriver;
         }
     }
 
-    private WebDriver createChromeDriver() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        WebDriverManager.chromedriver().setup();
-        return new ChromeDriver(options);
-    }
-
-    private WebDriver createFirefoxDriver() {
-        WebDriverManager.firefoxdriver().setup();
-        return new FirefoxDriver();
-    }
-
-    private WebDriver createEdgeDriver() {
-        WebDriverManager.edgedriver().setup();
-        return new EdgeDriver();
-    }
-
     private String setDefaultBrowser(String browser) {
-        if (Objects.isNull(browser) || browser.isEmpty()) {
-            return "chrome";
+        if(Objects.isNull(browser) || browser.isEmpty()) {
+            browser = "chrome";
         }
         return browser;
     }
